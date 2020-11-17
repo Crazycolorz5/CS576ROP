9936aa2a8ef3a01b41d4be83e7f06ee7899bbc71
Adam Chen <Crazycolorz5@gmail.com>
2020-11-17 16:57:05 -0500

    Begun work on Gadget class and function for finding gadgets.

--- gadget.py
+++ gadget.py
@@ -1,1 +1,21 @@
+from capstone import *
 
+def format_bytes(bs):
+    return ' '.join([hex(b) for b in bs])
+
+class Gadget():
+    # Fields
+    # self.insns: array of capstone CsInsn's that represent the gadget
+    def __init__(self, insns):
+        self.insns = insns
+        
+    def prettyPrint(self):
+        acc = ''
+        for insn in self.insns:
+            acc += '{}: {} {} {}\n'.format(insn.address, format_bytes(insn.bytes), insn.mnemonic, insn.op_str)
+        return acc
+
+# In code segments locate ret's or indirect calls and search preceeding instructions for useful behaviors (movs, pushes, pops, incs)
+# [CsInsn] -> [Int] (indices of terminators)
+def findGadgetTerminators(code):
+    pass
