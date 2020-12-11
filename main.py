87daf040bffabc6bfacd2feb99a8da9ce14ded99
Adam Chen <Crazycolorz5@gmail.com>
2020-12-10 19:39:17 -0500

    Test renamed to main; testing functions removed so only used code remains.

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-# Utility functions for testing
-from arch import md
 from elf import *
 from gadget import *
 from execveChain import *
 
-def printCode(c):
-    for (address, size, mnemonic, op_str) in md.disasm_lite(c, 0):
-        print("0x%x:\t%s\t%s" %(address, mnemonic, op_str))
-
-def printAll(s):
-    elf = Elf(s)
-    texts = elf.getTextSegments()
-    for t in texts:
-        printCode(t.data)
-        print()
-
-def printAllGadgets(s):
-    e = Elf(s)
-    gs = getAllGadgets(e)
-    for g in gs:
-        print(g)
-
 def getAllGadgets(e):
     return [g for t in e.getTextSegments() for cb in t.getCodeBlocks() for g in extractAllGadgets(cb)]
-
-def printGadgetsInSegments(ts):
-    for t in ts:
-        for g in t:
-            print(g)
 
 def ROPchainBinary(s):
     elf = Elf(s)
